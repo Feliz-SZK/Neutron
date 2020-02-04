d87d8e9ddfa15696c8f17bf2553a28469c9ebe65
Salim <41662860+Feliz-SZK@users.noreply.github.com>
2020-02-05 00:59:14 +0530

    For Arch based distros
    traffic anonymizer for arch

---
+++ Neutron-A/installer.sh
@@ -0,0 +1,21 @@
+if [ $(id -u) -ne 0 ]; then
+	echo "This script must be ran as root"
+	exit 1
+fi
+
+
+clear
+echo " "
+echo "******* Neutron installer ********"
+echo " "
+echo " >>>>> Installing Modules"
+sudo pacman -S tor -y -qq
+sudo pacman -S resolvconf -y -qq
+sleep 0.5
+echo " >>>>> Installing Neutron "
+sudo cp neutron /usr/bin/neutron
+sudo chmod +x /usr/bin/neutron
+sleep 0.5
+echo " :::::::: [Done] :::::::: "
+sleep 0.5
+echo " >>>>> Fire up your terminal and type in 'neutron' for usage "
